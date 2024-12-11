4f86988914a08336c9bba24f22f77761d67a72a9
shifengren <1944832385@qq.com>
2024-12-11 17:03:00 +0800

    rust use bacon-ls

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,4 +12,4 @@ vim.g.autoformat = false
 -- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
 -- only for diagnostics. The rest of LSP support will still be
 -- provided by rust-analyzer.
--- vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+vim.g.lazyvim_rust_diagnostics = "bacon-ls"
