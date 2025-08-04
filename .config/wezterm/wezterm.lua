9b7a66aea310e205f5718a04248f27d830859008
shifengren <1944832385@qq.com>
2025-08-04 15:26:21 +0800

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,9 @@ local config = wezterm.config_builder()
 -- color scheme
 config.color_scheme = 'tokyonight_moon'
 -- tab
+config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 -- window
 config.window_decorations = "RESIZE"
 return config
