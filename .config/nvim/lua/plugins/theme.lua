61dc5dbc5d2e596a7fdd2feaa6b961e5ca8643fb
shifengren <1944832385@qq.com>
2026-03-17 11:47:36 +0800

    update nvim theme: tokyonight

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -18,13 +18,13 @@ return {
       },
     },
   },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false, opts = { flavour = "mocha" } },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false, opts = { flavour = "mocha" } },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
 
   -- {
   --   "nvim-lualine/lualine.nvim",
